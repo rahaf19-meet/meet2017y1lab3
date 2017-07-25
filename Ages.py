be56bd20eceb04fa9e38bbc1dce4600c827df5e6
rahaf19-meet <rahaf19@meet.mit.edu>
2017-07-25 17:33:09 +0300

    Stopping here

---
+++ Ages.py
@@ -0,0 +1,9 @@
+print('How old are you?')
+age1=input()
+print('How old are you?')
+age2=input()
+print(int(age1)+int(age2))
+
+
+
+print(age1==age2)
---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+print("Γειά σου Κόσμε is Hello World in Greek")
+print("Olá Mundo is Hello World in Portoguese")
+print("Ahoj světe is Helo World in Czech")
+print("안녕 세상 is Helllo World in Korean")
+print("你好，世界 is llo World in Chinese")
+print("नमस्ते दुनिया is Hello World in Hindi")
+print("salamu Dunia is Hello World in Sawahili")
+
+
